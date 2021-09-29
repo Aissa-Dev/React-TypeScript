ebd8558c479c812a797221ce59fbfe5c647bb087
aissamihi <aissamihi@gmail.com>
2021-09-29 19:04:34 -0400

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Button } from "./components/Button";
-import { Greet } from "./components/Greet";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
-import { Status } from "./components/Status";
+import { Container } from "./components/Container";
 
 function App() {
-  const personName = {
-    firstName: "aissa",
-    lastName: "mihi"
-  };
-
-  const handleClick = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    id: number
-  ) => {
-    console.log("clicked passed from app xD", event, id);
-  };
-  const nameList = [
-    {
-      first: "aissa",
-      last: "mihi"
-    },
-    {
-      first: "mouad",
-      last: "balegh"
-    },
-    {
-      first: "hachimi",
-      last: "djalil"
-    }
-  ];
   return (
     <div className="App">
-      <Greet name="aissa mihi" isLoggeIn={true} messageCount={10} />
-      <Greet name="aissa mihi" isLoggeIn={true} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      <Status status="loading" />
-      <Heading x={10}> Placeholder text from app.tsx</Heading>
-      <Oscar>
-        <Heading x={16}> Placeholder text from app.tsx</Heading>
-      </Oscar>
-      <Button handleClick={handleClick} />
+      <Container
+        styles={{
+          color: "green",
+          padding: "1rem",
+          border: "1px solid black",
+          fontWeight: "bolder"
+        }}
+      />
     </div>
   );
 }
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,6 @@
-export const Container = () => {
-  return <div>text content goes here</div>;
+type ContainerProps = {
+  styles: React.CSSProperties;
+};
+export const Container = (props: ContainerProps) => {
+  return <div style={props.styles}>text content goes here</div>;
 };
